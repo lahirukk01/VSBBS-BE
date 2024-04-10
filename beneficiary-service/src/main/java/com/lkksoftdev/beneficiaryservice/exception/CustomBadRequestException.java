bfeef246bc5490730ffe13262e380f2eb3f49648
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2024-04-11 00:06:13 +0530

    Added exceptions

---
+++ beneficiary-service/src/main/java/com/lkksoftdev/beneficiaryservice/exception/CustomBadRequestException.java
@@ -0,0 +1,11 @@
+package com.lkksoftdev.beneficiaryservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class CustomBadRequestException extends RuntimeException{
+    public CustomBadRequestException(String message) {
+        super(message);
+    }
+}
