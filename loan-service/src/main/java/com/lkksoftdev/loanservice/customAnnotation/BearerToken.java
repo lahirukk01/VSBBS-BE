7321853b117051ce8ea71e73a860d52754e9ab2d
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2024-04-11 18:06:10 +0530

    Bearer token extraction removed

--- loan-service/src/main/java/com/lkksoftdev/loanservice/customAnnotation/BearerToken.java
+++
@@ -1,9 +0,0 @@
-package com.lkksoftdev.loanservice.customAnnotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface BearerToken {
-}
