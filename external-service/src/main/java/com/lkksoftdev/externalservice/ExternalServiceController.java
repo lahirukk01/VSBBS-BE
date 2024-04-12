9883c19178d5a20e330b59cc55ccf21e22559773
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2024-04-12 22:43:04 +0530

    Using reactive programming

--- external-service/src/main/java/com/lkksoftdev/externalservice/ExternalServiceController.java
+++ external-service/src/main/java/com/lkksoftdev/externalservice/ExternalServiceController.java
@@ -9,21 +9,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ExternalServiceController {
     private final Logger LOGGER = LoggerFactory.getLogger(ExternalServiceController.class);
 
     @PostMapping("/payments/card")
-    public ResponseEntity<CardPaymentResponseDto> makeCardPayment(CardPaymentDetailsDto cardPaymentDetailsDto) {
+    public Mono<ResponseEntity<CardPaymentResponseDto>> makeCardPayment(CardPaymentDetailsDto cardPaymentDetailsDto) {
         LOGGER.info("Processing card payment for customer: {}", cardPaymentDetailsDto.toString());
-        return ResponseEntity.ok(new CardPaymentResponseDto("OK"));
+        return Mono.just(ResponseEntity.ok(new CardPaymentResponseDto("OK")));
     }
 
     @PostMapping("/credit-ratings")
-    public ResponseEntity<CreditRatingResponseDto> getCreditRating(CustomerDto customerDto) {
+    public Mono<ResponseEntity<CreditRatingResponseDto>> getCreditRating(CustomerDto customerDto) {
         LOGGER.info("Getting credit rating for customer: {}", customerDto.toString());
-        return ResponseEntity.ok(new CreditRatingResponseDto(250));
+        return Mono.just(ResponseEntity.ok(new CreditRatingResponseDto(250)));
     }
-
 }
