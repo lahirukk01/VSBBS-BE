46b61d195924eb6599dd37a79d056846c9ae7cdb
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2024-05-19 20:08:21 +0530

    Beneficiary status enum refactored

--- beneficiary-service/src/main/java/com/lkksoftdev/beneficiaryservice/beneficiary/BeneficiaryStatus.java
+++ beneficiary-service/src/main/java/com/lkksoftdev/beneficiaryservice/beneficiary/BeneficiaryStatus.java
@@ -4,18 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum BeneficiaryStatus {
-//    PENDING("PENDING"),
-//    APPROVED("APPROVED"),
-//    REJECTED("REJECTED");
     PENDING,
     APPROVED,
     REJECTED;
-
-//    private final String status;
-//
-//    BeneficiaryStatus(String status) {
-//        this.status = status;
-//    }
 
     public static boolean isValidStatus(String status) {
         for (BeneficiaryStatus beneficiaryStatus : BeneficiaryStatus.values()) {
