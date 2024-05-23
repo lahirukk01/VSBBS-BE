f4d1e90575612541588e0d5c9c7d271a0fe9b4b6
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2024-05-23 17:06:19 +0530

    Added script with commands to spin up servers from jar files.

--- launchJars.sh
+++ launchJars.sh
@@ -1,2 +1,9 @@
-nohup java -jar eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar &
-nohup java -jar config-server/target/config-server-0.0.1-SNAPSHOT.jar &
+nohup java -jar eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar > eureka-server/output.log &
+nohup java -jar config-server/target/config-server-0.0.1-SNAPSHOT.jar > config-server/output.log &
+
+nohup java -jar registration-service/target/registration-service-0.0.1-SNAPSHOT.jar > registration-service/output.log &
+nohup java -jar account-service/target/account-service-0.0.1-SNAPSHOT.jar > account-service/output.log &
+nohup java -jar beneficiary-service/target/beneficiary-service-0.0.1-SNAPSHOT.jar > beneficiary-service/output.log &
+nohup java -jar loan-service/target/loan-service-0.0.1-SNAPSHOT.jar > loan-service/output.log &
+nohup java -jar external-service/target/external-service-0.0.1-SNAPSHOT.jar > external-service/output.log &
+nohup java -jar api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar > api-gateway/output.log &
